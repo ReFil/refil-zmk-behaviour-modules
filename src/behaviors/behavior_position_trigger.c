e606184602ee2099041e2b20b73a5cf85c2dd930
ReFil <31960031+ReFil@users.noreply.github.com>
2025-12-20 15:42:59 +0000

    key position trigger

---
+++ src/behaviors/behavior_position_trigger.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2025 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_position_trigger
+
+#include <zephyr/device.h>
+#include <drivers/behavior.h>
+#include <zephyr/logging/log.h>
+
+#include <zmk/event_manager.h>
+#include <zmk/behavior_queue.h>
+#include <zmk/virtual_key_position.h>
+#include <zmk/events/position_state_changed.h>
+#include <zmk/keymap.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+
+static const struct behavior_parameter_value_metadata param_values[] = {
+    {
+        .display_name = "Position",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_RANGE,
+        .range = {.min = 0, .max = 150},
+    },
+};
+
+static const struct behavior_parameter_metadata_set param_metadata_set[] = {{
+    .param1_values = param_values,
+    .param1_values_len = ARRAY_SIZE(param_values),
+}};
+
+static const struct behavior_parameter_metadata metadata = {
+    .sets_len = ARRAY_SIZE(param_metadata_set),
+    .sets = param_metadata_set,
+};
+
+#endif
+
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d", binding->param1);
+    return raise_zmk_position_state_changed(
+            (struct zmk_position_state_changed){.source = ZMK_POSITION_STATE_CHANGE_SOURCE_LOCAL,
+                                                .state = true,
+                                                .position = binding->param1,
+                                                .timestamp = event.timestamp});
+}
+
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    LOG_DBG("release position %d", binding->param1);
+    return raise_zmk_position_state_changed(
+            (struct zmk_position_state_changed){.source = ZMK_POSITION_STATE_CHANGE_SOURCE_LOCAL,
+                                                .state = false,
+                                                .position = binding->param1,
+                                                .timestamp = event.timestamp});
+}
+
+static const struct behavior_driver_api behavior_position_trigger_driver_api = {
+    .binding_pressed = on_keymap_binding_pressed,
+    .binding_released = on_keymap_binding_released,
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+    .parameter_metadata = &metadata,
+#endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+};
+
+#define KP_INST(n)                                                                                 \
+    BEHAVIOR_DT_INST_DEFINE(n, NULL, NULL, NULL, NULL, POST_KERNEL,                                \
+                            CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_position_trigger_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(KP_INST)
