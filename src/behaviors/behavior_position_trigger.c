90e979e155a3834b604e513f1a90118dbfbb1760
Willow Herring <31960031+ReFil@users.noreply.github.com>
2026-02-21 20:20:00 +0000

    Revert "Change working logic to try and speed up"
    This reverts commit 804fbbcadcf1f66f84eeec46e7745ae347a5d549.

--- src/behaviors/behavior_position_trigger.c
+++ src/behaviors/behavior_position_trigger.c
@@ -44,9 +44,11 @@ static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
     LOG_DBG("position %d", binding->param1);
     if(binding-> param1 != event.position)
-        return zmk_behavior_invoke_binding(zmk_keymap_get_layer_binding_at_idx(zmk_keymap_highest_layer_active(), binding->param1),
-         (struct zmk_behavior_binding_event){.layer = zmk_keymap_highest_layer_active(), .position = binding->param1, .timestamp = event.timestamp}, true);
-
+        return raise_zmk_position_state_changed(
+            (struct zmk_position_state_changed){.source = ZMK_POSITION_STATE_CHANGE_SOURCE_LOCAL,
+                                                .state = true,
+                                                .position = binding->param1,
+                                                .timestamp = event.timestamp});
     else
         return ZMK_BEHAVIOR_OPAQUE;
 }
@@ -55,9 +57,11 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
                                       struct zmk_behavior_binding_event event) {
     LOG_DBG("release position %d", binding->param1);
     if(binding-> param1 != event.position)
-        return zmk_behavior_invoke_binding(zmk_keymap_get_layer_binding_at_idx(zmk_keymap_highest_layer_active(), binding->param1),
-         (struct zmk_behavior_binding_event){.layer = zmk_keymap_highest_layer_active(), .position = binding->param1, .timestamp = event.timestamp}, false);
-
+        return raise_zmk_position_state_changed(
+            (struct zmk_position_state_changed){.source = ZMK_POSITION_STATE_CHANGE_SOURCE_LOCAL,
+                                                .state = false,
+                                                .position = binding->param1,
+                                                .timestamp = event.timestamp});
     else
         return ZMK_BEHAVIOR_OPAQUE;
 }
